187f422eb7d4903a31c48dc76fcccb7c359da3fa
worlddeleteRin <worlddelete0@gmail.com>
2022-04-02 21:59:32 +0300

    add topic url parsing

--- group/main.py
+++ group/main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from ok_core.client import OkClient
 
 
@@ -8,3 +9,15 @@ class OkGroup:
         client: OkClient
     ):
         self.client = client
+
+    def parseGroupTopicIdsFromUrl(
+        self,
+        url: str
+    ) -> tuple[str, str] | None:
+        # https://ok.ru/group/61638264422477/topic/154623547495757
+        try:
+            g = url.split('group/')[1].split('/')[0]
+            t = url.split('topic/')[1]
+        except:
+            return None
+        return (g,t)
