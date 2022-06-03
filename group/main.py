1c59b44e832aca72f423c3920d5d34c2d70dc27b
worlddeleteRin <worlddelete0@gmail.com>
2022-06-03 04:24:49 +0300

    :sparkles: improve group parser

--- group/main.py
+++ group/main.py
@@ -14,10 +14,22 @@ class OkGroup:
     def parseGroupTopicIdsFromUrl(
         url: str
     ) -> tuple[str, str] | None:
-        # https://ok.ru/group/61638264422477/topic/154623547495757
-        try:
-            g = url.split('group/')[1].split('/')[0]
-            t = url.split('topic/')[1]
-        except:
-            return None
-        return (g,t)
+        if 'group' in url:
+            # supposed url to be like
+            # https://ok.ru/group/61638264422477/topic/154623547495757
+            try:
+                g = url.split('group/')[1].split('/')[0]
+                t = url.split('topic/')[1]
+            except:
+                return None
+            return (g,t)
+        else:
+            # supposed url to be like
+            # https://ok.ru/kinofan.movies/topic/154853050242381
+            try:
+                g = url.split('ok.ru/')[1].split('/')[0]
+                t = url.split('topic/')[1]
+                pass
+            except:
+                return None
+            return (g,t)
